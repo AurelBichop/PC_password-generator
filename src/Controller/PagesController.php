816daae0fa57c8e81cae37deefb76901783f6b47
Aurelien <linux.aurelien@gmail.com>
2022-01-21 00:04:10 +0100

    Add form

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,14 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PagesController extends AbstractController
 {
     #[Route('/pages', name: 'pages')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        dump($request->query->all());
+
         return $this->render('pages/pages.html.twig');
     }
 }
